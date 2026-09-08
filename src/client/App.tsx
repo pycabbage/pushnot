9ac77b18fe5c1ef0aebf51ef123a76d4088d7631
kotaro.higuchi <33714346+pycabbage@users.noreply.github.com>
2026-09-08 14:56:19 +0900

    feat: add variable font support and enhance typesetting styles

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -111,6 +111,9 @@ export default function App(props: AppProps) {
 
   return (
     <div {...props}>
+      <div className="typeset typeset-docs max-w-[37em]">
+        <h1 className="font-mono">pushnot</h1>
+      </div>
       <p>Session ID: {props["data-session-id"]}</p>
       <Button onClick={handleToggleRegistration} disabled={isPending}>
         {registerLabel}
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
+  server: {
+    hmr: true,
+  },
   plugins: [
     cloudflare(),
     ssrPlugin(),
